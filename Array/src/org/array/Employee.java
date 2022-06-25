8a1df1c3fc53bdca520e128e3348dbbd53e70ac0
vimal0316 <108186925+vimal0316@users.noreply.github.com>
2022-06-25 20:24:50 +0530

    Add files via upload

---
+++ Array/src/org/array/Employee.java
@@ -0,0 +1,28 @@
+package org.array;
+
+public class Employee {
+	public static void main(String[] args) {
+	int a[]=new int[5];
+		
+		a[0]=10;
+		a[1]=20;
+		a[2]=30;
+		a[3]=40;
+		a[4]=50;
+	
+		int b=a.length;
+		System.out.println("length is"+b);
+			
+		
+      for (int i : a) {
+    	  System.out.println(i);
+		
+	}				
+			}
+	
+			
+		}
+		
+	
+
+
